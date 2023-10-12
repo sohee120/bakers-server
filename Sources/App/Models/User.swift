cb54410c8b3f6ec879de5afd9cc5a3ec76a867eb
sohee120 <yuhsohee120@gmail.com>
2023-10-12 22:13:44 +0900

    Feat: User모델 추가

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,39 @@
+//
+//  User.swift
+//  
+//
+//  Created by 윤소희 on 2023/10/12.
+//
+
+import Fluent
+import Vapor
+
+final class Account: Model, Content {
+    static let schema = "accounts" //테이블 또는 컬렉션의 이름
+    
+    @ID(key: .id) //모델 인스턴스의 고유 식별자
+    var id: UUID?
+
+    @OptionalField(key: "recent")
+    var recent: [String]?
+    
+    @OptionalField(key: "save")
+    var save: [String]?
+    
+    @OptionalField(key: "memo")
+    var memo: [String]?
+    
+    @Timestamp(key: "createdAt", on:.create)
+    var createdAt: Date?
+    
+
+    init() { }
+
+    init(id: UUID? = nil, recent: [String]?, save: [String]?, memo: [String]?, createdAt: Date? = nil) {
+        self.id = id
+        self.recent = recent
+        self.save = save
+        self.memo = memo
+        self.createdAt = createdAt
+    }
+}
