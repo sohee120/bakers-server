d24f65e2c9606342af3fb257bab409c14dfedbb6
sohee120 <yuhsohee120@gmail.com>
2023-10-13 13:59:01 +0900

    로컬 DB구성

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -20,16 +20,21 @@ public func configure(_ app: Application) async throws {
         // ...
     }
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+//    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+//        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+//        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+//        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+//        tls: .prefer(try .init(configuration: .clientDefault)))
+//    ), as: .psql)
 
     app.migrations.add(CreateTodo())
+    
+    app.databases.use(
+            .postgres(hostname: "localhost", username: "yunsohui", password: "", database: "bakers"),
+            as: .psql
+        )
 
     // register routes
     try routes(app)
